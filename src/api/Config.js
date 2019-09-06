077a10ff85e6b6462a36bc47174b45e476b9a50f
Phu D. Nguyen <Phu.Nguyen@sandata.com>
2019-09-06 19:32:23 +0700

    update

--- src/api/Config.js
+++ src/api/Config.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-  baseUrl: 'http://192.168.137.17:8080/api',
+  baseUrl: 'http://192.168.1.101:8080/api',
 };
 
 export default Object.assign({}, apiConfig);
--- src/component/FacePreview.js
+++ src/component/FacePreview.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button/Button";
 import api from "../api/Api"
-import TextField from "@material-ui/core/TextField/TextField";
 
 class FacePreview extends React.Component {
     state = {
@@ -298,62 +297,138 @@ class FacePreview extends React.Component {
     };
 
     componentDidMount = (props) => {
-        this.drawResult();
+        this.drawRecognizedResult();
     };
 
-    reload = () => {
+    detect = () => {
         const _this = this;
         this.setState({loading: true}, function () {
-            api.get("/recognize").then(resp => {
-                _this.setState({recognizeResult: resp}, _this.drawResult);
+            api.get("/detectFaces").then(resp => {
+                _this.setState({detectResult: resp}, _this.drawDetectResult);
             }).finally(() => {
                 _this.setState({loading: false})
             })
         });
-
     };
 
-    drawResult = () => {
+    recognize = () => {
+        const _this = this;
+        this.setState({loading: true}, function () {
+            api.get("/recognizeFaces").then(resp => {
+                _this.setState({recognizeResult: resp}, _this.drawRecognizedResult);
+            }).finally(() => {
+                _this.setState({loading: false})
+            })
+        });
+    };
+
+    drawRecognizedResult = () => {
         const resp = this.state.recognizeResult;
         const canvas = document.getElementById("c");
         const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.font = "24px Roboto";
+        ctx.strokeStyle = "green";
+        ctx.lineWidth = "4";
         const img = new Image();
         img.onload = () => {
-            ctx.drawImage(img, 0, 0)
-            if (resp.faces) {
-                for (let i = 0; i < resp.faces.length; i++) {
-                    const face = resp.faces[i];
-                    console.log(face);
-                    const rect = face.Rectangle;
+            ctx.drawImage(img, 0, 0);
+            if (resp.recognizedFaces && resp.recognizedFaces.length) {
+                for (let i = 0; i < resp.recognizedFaces.length; i++) {
+                    const face = resp.recognizedFaces[i];
+                    const rect = face.rect;
                     ctx.beginPath();
-                    ctx.lineWidth = "4";
-                    ctx.strokeStyle = "green";
+
                     ctx.rect(rect.Min.X, rect.Min.Y, rect.Max.X - rect.Min.X, rect.Max.Y - rect.Min.Y);
                     ctx.stroke();
+                    if (face.label) {
+                        ctx.fillStyle = "green";
+                        ctx.fillText(face.label, rect.Min.X + 8, rect.Max.Y - 8);
+                    } else {
+                        ctx.fillStyle = "yellow";
+                        ctx.fillText("UNKNOWN", rect.Min.X + 8, rect.Max.Y - 8);
+                    }
                 }
             }
         };
         img.src = "data:image/png;base64," + resp.image
     };
+
+    drawDetectResult = () => {
+        const resp = this.state.detectResult;
+        const canvas = document.getElementById("c");
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = "24px Roboto";
+        ctx.strokeStyle = "green";
+        ctx.lineWidth = "4";
+        const img = new Image();
+        img.onload = () => {
+            ctx.drawImage(img, 0, 0);
+            if (resp.detectedFaces && resp.detectedFaces.length) {
+                for (let i = 0; i < resp.detectedFaces.length; i++) {
+                    const face = resp.detectedFaces[i];
+                    const rect = face.rect;
+                    ctx.beginPath();
+                    ctx.rect(rect.Min.X, rect.Min.Y, rect.Max.X - rect.Min.X, rect.Max.Y - rect.Min.Y);
+                    ctx.stroke();
+                    if (face.label) {
+                        ctx.fillStyle = "green";
+                        ctx.fillText(face.label, rect.Min.X + 8, rect.Max.Y - 8);
+                    }
+                }
+            }
+        };
+        img.src = "data:image/png;base64," + resp.image
+    };
+
     onCanvasClick = (e) => {
+        if (!this.state.detectResult) {
+            return;
+        }
         const rect = e.target.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
-        console.log(x, y);
-        const faces = this.state.recognizeResult.faces;
+        const faces = this.state.detectResult.detectedFaces;
         if (faces && faces.length > 0) {
             for (let i = 0; i < faces.length; i++) {
-                const rect = faces[i].Rectangle;
+                const rect = faces[i].rect;
                 if (rect.Min.X < x && x < rect.Max.X && rect.Min.Y < y && y < rect.Max.Y) {
                     const user = window.prompt("Enter user name");
-                    const ctx = e.target.getContext("2d");
-                    ctx.fillStyle = "green";
-                    ctx.fillText(user, rect.Min.X + 8, rect.Max.Y - 8);
+                    if (user) {
+                        faces[i].label = user;
+                    }
+                    this.drawDetectResult()
                 }
             }
         }
 
+    };
+
+    submit = () => {
+        if (this.state.detectResult && this.state.detectResult.detectedFaces) {
+            const faces = this.state.detectResult.detectedFaces;
+            let labels = [];
+            for (let i = 0; i < faces.length; i++) {
+                const face = faces[i];
+                if (face.label) {
+                    labels.push({
+                        "label": face.label,
+                        "descriptor": face.descriptor,
+                    })
+                }
+            }
+            if (labels.length > 0) {
+                this.setState({loading: true}, function () {
+                    api.post("/labels", labels).then(resp => {
+                        console.log("submitted");
+                    }).finally(() => {
+                        this.setState({loading: false});
+                    })
+                });
+
+            }
+        }
     };
 
     render = () => {
@@ -362,35 +437,34 @@ class FacePreview extends React.Component {
             <div>
                 <canvas id="c" width={640} height={480} onClick={this.onCanvasClick}/>
                 <br/>
-                <Button onClick={this.reload}
+                <Button onClick={this.detect}
                         disabled={this.state.loading}
                         variant="contained"
+                        style={{margin: 8}}
                         color="primary"
                 >
-                    Reload
+                    Detect
+                </Button>
+
+                <Button onClick={this.recognize}
+                        disabled={this.state.loading}
+                        variant="contained"
+                        style={{margin: 8}}
+                        color="primary"
+                >
+                    Recognize
+                </Button>
+
+                <Button onClick={this.submit}
+                        disabled={this.state.loading}
+                        variant="contained"
+                        style={{margin: 8}}
+                        color="primary">
+                    Save
                 </Button>
                 <br/>
-                {recognizeResult && recognizeResult.faces && recognizeResult.faces.length > 0 &&
-                <div>
-                    <h2>User In Pictures</h2>
-                    {recognizeResult.faces.map((face, idx) => {
-                        return (
-                            <div>
-                                <TextField placeholder={`${idx + 1}`}/>
-                            </div>
 
-                        )
-                    })}
-                    <Button onClick={this.submit}
-                            disabled={this.state.loading}
-                            variant="contained"
-                            color="primary"
-                            style={{margin: "8px"}}
-                    >
-                        Save
-                    </Button>
-                </div>
-                }
+
             </div>
         )
     }
